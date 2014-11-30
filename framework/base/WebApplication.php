454241449da5b374a2185d286e3294da326b62a9
Donie Leigh <donie.leigh@gmail.com>
2014-11-30 20:20:45 +0800

    add fatal error handler

--- framework/base/WebApplication.php
+++ framework/base/WebApplication.php
@@ -174,6 +174,15 @@ class WebApplication {
             set_error_handler(array($this, 'handleError'), error_reporting());
         if ($this->enableExceptionHandling == true)
             set_exception_handler(array($this, 'handleException'));
+        ini_set('display_errors', false);
+        register_shutdown_function(function(){
+            $error = error_get_last();
+            if (isset($error['type']) && in_array($error['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_CORE_WARNING, E_COMPILE_ERROR, E_COMPILE_WARNING])) {
+                ob_clean();
+                echo '<pre>'; var_dump($error); echo '</pre>';
+                exit(1);
+            }
+        });
     }
     
     /**
